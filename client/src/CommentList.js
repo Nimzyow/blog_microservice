c53d46475da78bab2d013dd4f6b9f8ddb2deada7
Nimzyow <n_soufiani@hotmail.com>
2020-06-04 22:23:37 +0100

    comments now show status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,10 +1,22 @@
 import React from "react";
 
-
 export const CommentList = ({ comments }) => {
-
   const renderComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === "pending") {
+      content = "This comment is await approval";
+    }
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderComments}</ul>;
