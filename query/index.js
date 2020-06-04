9e2402e0b30dd1aa322317da9f213d3e29090b88
Nimzyow <n_soufiani@hotmail.com>
2020-06-04 22:52:51 +0100

    event bus stores events in memory

--- query/index.js
+++ query/index.js
@@ -1,7 +1,16 @@
-const app = require("./server");
+const { app, handleEvent } = require("./server");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 4002;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server listening on ${PORT}`);
+
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (const event of res.data) {
+    console.log("Processing event:", event.type);
+
+    handleEvent(event.type, event.data);
+  }
 });
