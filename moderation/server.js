549548b3064d28abd4e4b812a80feb456868e56c
Nimzyow <n_soufiani@hotmail.com>
2020-06-04 22:03:36 +0100

    all working

--- moderation/server.js
+++ moderation/server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const axios = require("Axios");
+const axios = require("axios");
 
 const app = express();
+app.use(bodyParser.json());
 
-app.unsubscribe(bodyParser.json());
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
 
-app.post("/events", (req, res) => {});
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+
+  res.send({});
+});
 
 module.exports = app;
--- query/server.js
+++ query/server.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -18,17 +17,30 @@ app.post("/events", (req, res) => {
 
   if (type === "PostCreated") {
     const { id, title } = data;
+
     posts[id] = { id, title, comments: [] };
   }
-  1;
 
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
+  console.log(posts);
 
   res.send({});
 });
