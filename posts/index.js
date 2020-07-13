558e9d4d22f7ce194f1f0227ec4c25a62a5c4fd7
Nimzyow <n_soufiani@hotmail.com>
2020-07-13 19:34:51 +0100

    created kubernetes service

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,8 @@
-const app = require("./server")
+const app = require("./server");
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
+  console.log("V55");
   console.log(`Listening on ${PORT}`);
 });
