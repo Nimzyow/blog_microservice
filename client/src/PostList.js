0fef974098be1d019ecf130f318b02f62beeb4c2
Nimzyow <n_soufiani@hotmail.com>
2020-07-17 17:54:32 +0100

    added unique posts route for ingress

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -14,7 +14,7 @@ export const PostList = () => {
   console.log(posts);
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4002/posts");
+    const res = await axios.get("http://posts.com/posts");
 
     setPosts(res.data);
   };
