d371c4907a41cded2e76f01a4ecdd3969741b36d
Nimzyow <n_soufiani@hotmail.com>
2020-07-17 16:42:41 +0100

    different services now point to associated clusterIPs

--- query/index.js
+++ query/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 4002;
 app.listen(PORT, async () => {
   console.log(`Server listening on ${PORT}`);
 
-  const res = await axios.get("http://localhost:4005/events");
+  const res = await axios.get("http://event-bus-srv:4005/events");
 
   for (const event of res.data) {
     console.log("Processing event:", event.type);
