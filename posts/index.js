c8d979cab3164e19f17238db118b15fa1e8b3391
Nimzyow <n_soufiani@hotmail.com>
2020-07-18 21:01:04 +0100

    scaffold succesfully implemented

--- posts/index.js
+++ posts/index.js
@@ -3,6 +3,6 @@ const app = require("./server");
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log("V55");
+  console.log("V10000trillion");
   console.log(`Listening on ${PORT}`);
 });
